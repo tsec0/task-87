5237bb8fec7748cf63ce45b096a05f59d697d818
Tsvetomir Dikov <tsvetomir.dikov@gmail.com>
2022-12-23 23:36:30 +0200

    fix: added 'ramda' package
    Added class list items from arrayToPluck with pluck ramda

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 import "../scss/app.scss";
+// const R = require("ramda");
+// const { pluck } = R;
+import * as R from 'ramda';
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -9,4 +12,9 @@ window.addEventListener("DOMContentLoaded", () => {
     { job: "programmer", class: "is-danger" },
   ];
   const articles = document.querySelectorAll("article");
+  const pluck = R.pluck('class', arrayToPluck);
+  
+  for (let index = 0; index < articles.length; index++) {
+    articles[index].classList.add(pluck[index]);
+  }
 });
